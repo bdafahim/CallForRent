398dc2f2caae7625393080d39a8a329e2d9bc4dc
bdafahim <badrudduza20@student.sust.edu>
2019-12-16 14:47:22 +0600

    authentication app User-manager model created

--- CallForRent/settings.py
+++ CallForRent/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'authentication'
 ]
 
+
+AUTH_USER_MODEL = 'authentication.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
