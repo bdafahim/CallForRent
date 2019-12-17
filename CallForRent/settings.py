802654db0be5dafe78e894fca390fc460b8a57ef
bdafahim <badrudduza20@student.sust.edu>
2019-12-17 23:51:18 +0600

    queryset added in posts view

--- CallForRent/settings.py
+++ CallForRent/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'CallForRent.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 def posts_create(request):
     return HttpResponse("<h1>Create</h1>")
 def posts_detail(request):
     return HttpResponse("<h1>detail</h1>")
 def posts_list(request):
-    return HttpResponse("<h1>list</h1>")
+    queryset = Post.objects.all()
+    context = {
+        "object_list": queryset,
+        "title": "List"
+    }
+    return render(request, 'posts/index.html', context)
 def posts_update(request):
     return HttpResponse("<h1>update</h1>")
 def posts_delete(request):
