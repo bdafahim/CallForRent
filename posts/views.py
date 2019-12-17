918394993cf03f97b602b2a91902044cbb653db7
bdafahim <badrudduza20@student.sust.edu>
2019-12-18 03:09:31 +0600

    post form template created and can post content by frontend

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 def posts_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        print(form.cleaned_data.get("title"))
+        instance.save()
+    # if request.method == "POST":
+    #     print(request.POST)
+    context = {
+        "form": form,
+    }
+    return render(request, 'posts/post_form.html', context)
 def posts_detail(request,id=None):
     #instance = Post.objects.get(id=5)
     instance = get_object_or_404(Post, id=id)
