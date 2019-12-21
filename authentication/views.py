cba896bcdbfe85994644ee30f49eca66e87be336
bdafahim <badrudduza20@student.sust.edu>
2019-12-21 19:02:02 +0600

    Login part complete

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def SignInView(request):
-    return render(request, 'authentication/login.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Succesfully loged in")
+            return redirect('posts:list')
+
+        else:
+            messages.success(request, "Wrong Email or password")
+            return redirect('authentication:signin')
+
+    else:
+        return render(request, 'authentication/login.html')
