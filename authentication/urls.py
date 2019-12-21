56999dd11c74a64767eb708e6a42ee806a3e12ea
bdafahim <badrudduza20@student.sust.edu>
2019-12-21 17:50:59 +0600

    login view and login.html added

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 app_name = 'authentication'
 
 urlpatterns = [
-
+ path('signin/', views.SignInView, name='signin'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def SignInView(request):
+    return render(request, 'authentication/login.html')
