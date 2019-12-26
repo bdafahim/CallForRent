45bb67cad799800d031946a0ea6cda552b44c7f6
bdafahim <badrudduza20@student.sust.edu>
2019-12-26 22:48:18 +0600

    Signup view added

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def SignInView(request):
     if request.method == 'POST':
@@ -24,3 +25,64 @@ def LogoutView(request):
     logout(request)
     messages.success(request, "Succesfully logged out")
     return redirect('home:home')
+
+
+def SignUpView(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, email=email, password=password)
+            login(request, user)
+            messages.success(request, "Succesfully registered")
+            return redirect('posts:list')
+    else:
+        form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'authentication/register.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
